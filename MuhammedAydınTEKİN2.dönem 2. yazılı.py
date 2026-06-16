d65fc10486bb9c77833b4e21a24ea683d6da3496
mat3676786766-star <mat3676786766@gmail.com>
2026-06-16 17:48:22 +0300

    Update MuhammedAydınTEKİN2.dönem 2. yazılı.py

--- MuhammedAydınTEKİN2.dönem 2. yazılı.py
+++ MuhammedAydınTEKİN2.dönem 2. yazılı.py
@@ -1,4 +1,9 @@
-import streamlit as st
+import os
+try:
+    import cv2
+except ImportError:
+    os.system('pip install opencv-python-headless')
+    import cv2import streamlit as st
 import cv2
 import numpy as np
 import time
